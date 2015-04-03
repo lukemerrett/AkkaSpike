2b01088539c2b3da3d3e593604786f1b3bc27096
Luke Merrett <lukeamerrett@gmail.com>
2015-04-03 22:43:37 +0100

    Added the actor for handling the message

---
+++ AkkaSpike/Actors/GreetActor.cs
@@ -0,0 +1,19 @@
+﻿using Akka.Actor;
+using AkkaSpike.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkkaSpike.Actors
+{
+    public class GreetActor : ReceiveActor
+    {
+        public GreetActor()
+        {
+            Receive<Greet>(greet => 
+               Console.WriteLine("The program says '{0}'", greet.Message));
+        }
+    }
+}
