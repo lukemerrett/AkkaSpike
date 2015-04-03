d20f8eea47ebe3cec6f89514c2a0546983e5c7c4
Luke Merrett <lukeamerrett@gmail.com>
2015-04-03 22:42:08 +0100

    Added the message dto

---
+++ AkkaSpike/Messages/Greet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AkkaSpike.Messages
+{
+    public class Greet
+    {
+        public string Message { get; set; }
+    }
+}
