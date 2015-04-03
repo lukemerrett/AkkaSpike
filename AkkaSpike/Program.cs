7f169dfa9ee6ccda68a29963daa40bdd58de563a
Luke Merrett <lukeamerrett@gmail.com>
2015-04-03 22:50:27 +0100

    Added the tutorial code from the Akka wiki to set up a basic listener

--- AkkaSpike/Program.cs
+++ AkkaSpike/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using Akka;
 using Akka.Actor;
+using AkkaSpike.Actors;
+using AkkaSpike.Messages;
 
 namespace AkkaSpike
 {
@@ -13,7 +15,22 @@ namespace AkkaSpike
     {
         public static void Main(string[] args)
         {
+            // Create a new actor system (a container for your actors)
+            var system = ActorSystem.Create("GreetingSystem");
 
+            // Create your actor and get a reference to it.
+            // This will be an "ActorRef", which is not a reference to the actual actor instance but rather a client or proxy to it.
+            var greeter = system.ActorOf<GreetActor>("Greeter");
+
+            var greet = new Greet
+            {
+                Message = "Hello there"
+            };
+
+            // Send a message to the actor
+            greeter.Tell(greet);
+
+            Console.ReadLine();
         }
     }
 }
